08bfe898f242bb61653e73a27ea7f4f8bc7a23f9
mrkarezina <mrk.arezina@gmail.com>
2019-05-25 17:12:41 -0400

    cache tools requirment

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ NUMBER_OF_RELATED_ARTICLES = 3
 natural_language_function_base_url = "https://us-central1-graph-intelligence.cloudfunctions.net/{0}"
 
 # graph_url = "http://neo4j:Trebinje66@localhost:7474/db/data/"
-GRAPH_URL = "http://neo4j:Trebinje66@35.202.226.197:7474/db/data/"
+GRAPH_URL = "http://neo4j:Trebinje66@35.184.182.149:7474/db/data/"
 
 # article_downloader.py
 # If less than 100 tokens retry parsing the article not cleaning dom
--- tests/test_api.py
+++ tests/test_api.py
@@ -7,36 +7,35 @@ class TestIntegrations(TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    # TODO: Issue running these unit tests, something with the graph connection?
-    # def test_summary_related(self):
-    #     response = self.app.post('/summary-related', json={
-    #         "article_url": "http://www.physiciansnewsnetwork.com/ximed/study-hospital-physician-vertical-integration-has-little-impact-on-quality/article_257c41a0-3a11-11e9-952b-97cc981efd76.html",
-    #         "user_id": 1,
-    #         "processor_id": "language-processor-health"
-    #     })
-    #
-    #     self.assertEqual(response.status_code,
-    #                      200)
-    #
-    #     response = json.loads(response.data)
-    #
-    #     self.assertGreater(len(response["related"]),
-    #                        2)
-    #
-    # def test_explore(self):
-    #     response = self.app.post('/explore', json={
-    #         "article_url": "https://www.health.harvard.edu/blog/conflict-of-interest-in-medicine-2018100114940",
-    #         "user_id": 1,
-    #         "processor_id": "language-processor-health"
-    #     })
-    #
-    #     self.assertEqual(response.status_code,
-    #                      200)
-    #
-    #     response = json.loads(response.data)
-    #
-    #     self.assertGreater(len(response["related"]),
-    #                        2)
+    def test_summary_related(self):
+        response = self.app.post('/summary-related', json={
+            "article_url": "http://www.physiciansnewsnetwork.com/ximed/study-hospital-physician-vertical-integration-has-little-impact-on-quality/article_257c41a0-3a11-11e9-952b-97cc981efd76.html",
+            "user_id": 1,
+            "processor_id": "language-processor-health"
+        })
+
+        self.assertEqual(response.status_code,
+                         200)
+
+        response = json.loads(response.data)
+
+        self.assertGreater(len(response["related"]),
+                           2)
+
+    def test_explore(self):
+        response = self.app.post('/explore', json={
+            "article_url": "https://www.health.harvard.edu/blog/conflict-of-interest-in-medicine-2018100114940",
+            "user_id": 1,
+            "processor_id": "language-processor-health"
+        })
+
+        self.assertEqual(response.status_code,
+                         200)
+
+        response = json.loads(response.data)
+
+        self.assertGreater(len(response["related"]),
+                           2)
 
     def test_summary(self):
         response = self.app.post('/summary', json={
